46786bb09fe2026a608a221005dd961daeeb000a
Ahmed Usama <eng.ahmedusama@yahoo.com>
2025-06-09 19:19:34 +0300

    Init project

---
+++ bin/playground_design_patterns.dart
@@ -0,0 +1,5 @@
+import 'package:playground_design_patterns/playground_design_patterns.dart' as playground_design_patterns;
+
+void main(List<String> arguments) {
+  print('Hello world: ${playground_design_patterns.calculate()}!');
+}
---
+++ test/playground_design_patterns_test.dart
@@ -0,0 +1,8 @@
+import 'package:playground_design_patterns/playground_design_patterns.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
